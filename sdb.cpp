38e435c98351730f13c28742319be0fb05a320c0
Daniel Fordan <xzq381@yahoo.com>
2012-04-12 01:10:06 +0800

    Added get, set, and delete

--- sdb.cpp
+++ sdb.cpp
@@ -47,6 +47,34 @@ void save()
 	writer.close();
 }
 
+void set(string key, string value)
+{
+	data[key] = value;
+	keys.push_back(key);
+	cout << "<" << key << "," << value <<">" " was added.\n";
+}
+
+string get(string key)
+{
+	if( data.count(key) > 0 )
+	{
+		return data[key];
+	}
+	else
+		return "Key not found.";
+}
+
+void del(string key)
+{
+	if( data.count(key) > 0 )
+	{
+		data.erase(key);
+		cout << "<" << key << "," << data[key] <<">" " was deleted.\n";
+	}
+	else
+		cout << "Key does not exist.\n";
+}
+
 int main()
 {
 	init();
