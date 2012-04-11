a9ae705e7bd34f179711fe1337bda773fea74c35
Daniel Fordan <xzq381@yahoo.com>
2012-04-12 01:27:17 +0800

    Almost done. No error checking yet

--- sdb.cpp
+++ sdb.cpp
@@ -78,7 +78,45 @@ void del(string key)
 int main()
 {
 	init();
-	
+	bool c = 1;
+	while(c)
+	{
+		string command;
+		cin >> command;
+		if( command ==  "set")
+		{
+			string key;
+			string value;
+			cin >> key;
+			cin >> value;
+			set(key, value);
+		}
+		else if( command ==  "get")
+		{
+			string key;
+			cin >> key;
+			cout << get(key) << endl;
+		}
+		else if( command == "delete")
+		{
+			string key;
+			cin >> key;
+			del(key);
+		}
+		else if( command == "exit")
+		{
+			c = 0;
+		}
+		else
+		{
+			cout << "Usage:\n";
+			cout << "\tset key value to add or edit an entry\n";
+			cout << "\tget key to retrieve an entry\n";
+			cout << "\tdelete key to delete an entry\n";
+			cout << "\texit to quit the program\n";
+		}
+			
+	}
 	save();
 	return 0;
 }
